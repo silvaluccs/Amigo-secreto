ddb550b1c5379f914dd5b0b81690d946aa0aee49
Lucas <kucasss19@gmail.com>
2025-07-29 21:50:48 -0300

    implementação de função para adicionar amigo na lista

--- app.js
+++ app.js
@@ -5,4 +5,36 @@
  */
 let amigos = [];
 
+/**
+ * Adiciona um novo nome à lista de amigos.
+ * - Valida se o campo está vazio.
+ * - Valida se o nome já existe na lista e pede confirmação para adicionar novamente.
+ * - Adiciona o nome ao array 'amigos', limpa o campo de entrada e atualiza a lista exibida.
+ */
+function adicionarAmigo() {
+  let nomeAmigoInput = document.querySelector('input');
+
+  if (nomeAmigoInput.value === "") {
+    alert("Por favor, insira um nome.");
+    console.log("Por favor, insira um nome.");
+    return;
+  }
+
+  if (amigos.includes(nomeAmigoInput.value)) {
+    console.log('O amigo já existe na lista');
+
+    let confirmaAdicao = confirm(`O amigo ${nomeAmigoInput.value} já está na lista.\nTem certeza que quer adicionar?`);
+
+    if (!confirmaAdicao) return;
+  }
+
+  amigos.push(nomeAmigoInput.value);
+  nomeAmigoInput.value = '';
+
+  console.log(amigos);
+  atualizarListaDeAmigos();
+}
+
+
+
 
