e758ed806456b1aac987637e33586e892bcb472a
Lucas <kucasss19@gmail.com>
2025-07-29 21:50:05 -0300

    adicionar array para amigos

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o pr
+/**
+ * Array global que armazena os nomes dos amigos cadastrados.
+ * @type {string[]}
+ */
+let amigos = [];
+
+
